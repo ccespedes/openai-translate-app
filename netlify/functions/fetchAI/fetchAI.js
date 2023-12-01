e143ac7ec11f4565ead47fa08032afdcd6c080bd
Carlos Cespedes <cespedes88@gmail.com>
2023-11-30 20:08:43 -0800

    return only data

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -7,21 +7,21 @@ const openai = new OpenAI({
 
 const handler = async (event) => {
   try {
-    const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      // messages: messages,
-      messages: event.body,
-    })
+    // const response = await openai.chat.completions.create({
+    //   model: 'gpt-3.5-turbo',
+    //   messages: event.body,
+    // })
     // messages.push({
     //   role: 'system',
     //   content: response.choices[0].message.content,
     // })
     // console.log(response.choices[0].message.content)
     // console.log(messages)
-
+    const data = event.body
     return {
       statusCode: 200,
-      body: JSON.stringify({ reply: response.choices[0].message.content }),
+      body: JSON.stringify({ data }),
+      // body: JSON.stringify({ reply: response.choices[0].message.content }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
