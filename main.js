3983561a89c902b9571460e59e2e7d95fc6c0c54
Carlos Cespedes <cespedes88@gmail.com>
2023-11-30 18:19:50 -0800

    parse

--- main.js
+++ main.js
@@ -40,11 +40,11 @@ async function pingAi(text, language) {
       headers: {
         'Content-Type': 'text/plain',
       },
-      body: JSON.stringify(messages),
+      body: JSON.parse(messages),
     })
 
     const data = await res.json()
-    console.log('data: ', data)
+    console.log('ddata: ', data)
 
     // const response = await openai.chat.completions.create({
     //   model: 'gpt-3.5-turbo',
@@ -67,7 +67,7 @@ async function pingAi(text, language) {
   } catch (error) {
     // showError(error)
     // console.log(messages)
-    console.log('error: ', error)
+    console.log('eerror: ', error)
   }
 }
 
