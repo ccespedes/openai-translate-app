443f709ec5451a566bccc31a63d959513b75b1e1
Carlos Cespedes <cespedes88@gmail.com>
2023-11-30 19:59:44 -0800

    rm dangerouslyAllowBrowser

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -2,7 +2,7 @@ import OpenAI from 'openai'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
+  // dangerouslyAllowBrowser: true,
 })
 
 const handler = async (event) => {
